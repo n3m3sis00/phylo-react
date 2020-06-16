0015eecec209c35d4e6a348629b5cde220622d09
Colin <colin.diesh@gmail.com>
2020-06-16 11:42:42 -0400

    Use functional react hooks style for sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,44 +1,30 @@
-import React from 'react'
-import { withStyles } from "@material-ui/core/styles";
-import Drawer from '@material-ui/core/Drawer';
+import React, { useContext } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
 
-import AppContext from '../container/Store'
+import AppContext from "../container/Store";
 
-
-const styles = therme => ({
+const useStyles = makeStyles((theme) => ({
   side_div: {
-    width:350
+    width: 350,
   },
-});
+}));
 
-class Sidebar extends React.Component {
-    static contextType = AppContext
+function Sidebar(props) {
+  const context = useContext(AppContext);
+  const { node, isOpen, setOpen } = context;
+  const classes = useStyles();
 
-    constructor(props){
-        super(props);
-        this.state = {
-            data:null
-        }
-    }
-    componentDidMount(){
-
-    }
-
-    render(){
-        const { node, isOpen, setOpen } = this.context
-        const { classes } = this.props
-
-        return (
-            <React.Fragment key={'right'}>
-                    <Drawer anchor={'right'} open={isOpen} onClose={() => setOpen(false)}>
-                        <div className = {classes.side_div} >
-                            {node !== null ? <h4>{node.data.name.replace(/_/g, " ")}</h4> : null}
-                        </div>
-                    </Drawer>
-            </React.Fragment>
-        );
-    }
-
+  return (
+    <React.Fragment key={"right"}>
+      <Drawer anchor={"right"} open={isOpen} onClose={() => setOpen(false)}>
+        <div className={classes.side_div}>
+          {node !== null ? <h4>{node.data.name.replace(/_/g, " ")}</h4> : null}
+        </div>
+      </Drawer>
+    </React.Fragment>
+  );
 }
 
-export default withStyles(styles, { withTheme: true })(Sidebar)
+export default Sidebar;
+
