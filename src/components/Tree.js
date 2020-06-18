88c4fdc8152d7021f05c3aa735359d392cab30cb
Colin <colin.diesh@gmail.com>
2020-06-18 11:38:09 -0400

    Use indentation level 4

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -1,32 +1,29 @@
-import React from 'react'
+import React, { useContext, useEffect } from "react";
 import * as d3 from "d3";
-import { parseNewick } from './Utils'
-import { treeData, smalltree } from './life.js'
+import { parseNewick } from "./Utils";
 
-import AppContext from '../container/Store'
+import AppContext from "../container/Store";
 
+export default function Tree(props) {
+    const context = useContext(AppContext);
 
-export default class Tree extends React.Component {
-    static contextType = AppContext
+    const { setOpen, setNode } = context;
+    const { treeData } = props;
 
-    constructor(props) {
-        super(props);
-    }
+    useEffect(() => {
+        console.log("parsing data");
+        var data = parseNewick(treeData);
 
-    async componentDidMount() {
+        const cluster = d3.cluster().size([500, 500]);
 
-        const { setOpen, setNode } = this.context
-
-        var data = await parseNewick(treeData)
-
-        const cluster = d3.cluster()
-            .size([3800, 500])
-
-        const root = d3.hierarchy(data, d => d.branchset)
+        const root = d3.hierarchy(data, (d) => d.branchset);
 
         cluster(root);
 
-        const svg = d3.select("svg")
+        d3.selectAll("svg > *").remove();
+
+        const svg = d3
+            .select("svg")
             .attr("viewBox", [-10, -10, 954, 3810])
             .attr("font-family", "sans-serif")
             .attr("font-size", 10);
@@ -41,51 +38,58 @@ export default class Tree extends React.Component {
             }
             `);
 
-
         svg.append("g")
             .attr("fill", "none")
             .attr("stroke", "#000")
             .selectAll("path")
             .data(root.links())
             .join("path")
-            .attr("d", d =>
-                "M" + d.source.y + " " + d.source.x
-                + "V" + d.target.x
-                + "H" + d.target.y
-            )
+            .attr(
+                "d",
+                (d) =>
+                    "M" +
+                    d.source.y +
+                    " " +
+                    d.source.x +
+                    "V" +
+                    d.target.x +
+                    "H" +
+                    d.target.y
+            );
 
         svg.append("g")
             .selectAll("circle")
             .data(root.descendants())
             .join("circle")
-            .attr("cx", d => d.y)
-            .attr("cy", d => d.x)
-            .attr("fill", d => d.children ? "#555" : "#999")
+            .attr("cx", (d) => d.y)
+            .attr("cy", (d) => d.x)
+            .attr("fill", (d) => (d.children ? "#555" : "#999"))
             .attr("r", 3);
 
         svg.append("g")
             .selectAll("text")
             .data(root.leaves())
             .join("text")
-            .attr("x", d => d.y + 5)
-            .attr("y", d => d.x + 4)
-            .text(d => d.data.name.replace(/_/g, " "))
+            .attr("x", (d) => d.y + 5)
+            .attr("y", (d) => d.x + 4)
+            .text((d) => d.data.name.replace(/_/g, " "))
             .on("mouseover", mouseovered(true))
             .on("mouseout", mouseovered(false))
-            .on("click", d => { setOpen(true); setNode(d) });
-
+            .on("click", (d) => {
+                setOpen(true);
+                setNode(d);
+            });
 
         function mouseovered(active) {
             return function (d) {
                 d3.select(this).classed("label--active", active);
             };
         }
-    }
+    }, [setNode, setOpen, treeData]);
 
-    render() {
-
-        return <div>
+    return (
+        <div>
             <svg> </svg>
         </div>
-    }
+    );
 }
