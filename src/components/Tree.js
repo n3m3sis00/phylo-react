495e13e08784226a1f2ba44a17f03e7364115663
Dr. Shreya Srivastava <shreya2feb@gmail.com>
2020-06-18 15:49:29 +0530

    Right angle links

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import * as d3 from "d3";
-import {parseNewick} from './Utils'
-import {treeData} from './life.js'
+import { parseNewick } from './Utils'
+import { treeData, smalltree } from './life.js'
 
 import AppContext from '../container/Store'
 
@@ -9,105 +9,83 @@ import AppContext from '../container/Store'
 export default class Tree extends React.Component {
     static contextType = AppContext
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {
-
-        }
     }
 
-    async componentDidMount(){
-        
-        const {setOpen, setNode} = this.context
+    async componentDidMount() {
+
+        const { setOpen, setNode } = this.context
 
         var data = await parseNewick(treeData)
-        
+
         const cluster = d3.cluster()
-            .size([1800, 500])
-            .separation((a, b) => 1)
-        
-        const color = d3.scaleOrdinal()
-            .domain(["Bacteria", "Eukaryota", "Archaea"])
-            .range(d3.schemeCategory10)
+            .size([3800, 500])
 
-        function setColor(d) {
-            var name = d.data.name;
-            d.color = color.domain().indexOf(name) >= 0 ? color(name) : d.parent ? d.parent.color : null;
-            if (d.children) d.children.forEach(setColor);
-        }
-
-    var makeChart = () => {
         const root = d3.hierarchy(data, d => d.branchset)
-            .sum(d => d.branchset ? 0 : 1)
-            .sort((a, b) => (a.value - b.value) || d3.ascending(a.data.length, b.data.length));
-    
+
         cluster(root);
-        setColor(root);
-    
+
         const svg = d3.select("svg")
-            .attr("viewBox", [0, 0, 954, 1800])
+            .attr("viewBox", [-10, -10, 954, 3810])
             .attr("font-family", "sans-serif")
-            .attr("font-size", 10)
-            .call(d3.zoom().on("zoom", function () {
-                    svg.attr("transform", d3.event.transform)
-            }));
-                
-    
+            .attr("font-size", 10);
+
         svg.append("style").text(`
             .link--active {
                 stroke: #000 !important;
                 stroke-width: 1.5px;
             }
-            .link-extension--active {
-                stroke-opacity: .6;
-            }
             .label--active {
                 font-weight: bold;
             }
             `);
-    
-    
+
+
         svg.append("g")
             .attr("fill", "none")
             .attr("stroke", "#000")
             .selectAll("path")
             .data(root.links())
             .join("path")
-            .each(function(d) { d.target.linkNode = this; })
-            .attr("d", d3.linkHorizontal()
-                    .x(d => d.y)
-                    .y(d => d.x))
-            .attr("stroke", d => d.target.color);
-    
+            .attr("d", d =>
+                "M" + d.source.y + " " + d.source.x
+                + "V" + d.target.x
+                + "H" + d.target.y
+            )
+
         svg.append("g")
-          .selectAll("text")
-          .data(root.leaves())
-          .join("text")
+            .selectAll("circle")
+            .data(root.descendants())
+            .join("circle")
+            .attr("cx", d => d.y)
+            .attr("cy", d => d.x)
+            .attr("fill", d => d.children ? "#555" : "#999")
+            .attr("r", 3);
+
+        svg.append("g")
+            .selectAll("text")
+            .data(root.leaves())
+            .join("text")
             .attr("x", d => d.y + 5)
             .attr("y", d => d.x + 4)
             .text(d => d.data.name.replace(/_/g, " "))
             .on("mouseover", mouseovered(true))
             .on("mouseout", mouseovered(false))
-            .on("click", d=>{setOpen(true); setNode(d)});
-    
+            .on("click", d => { setOpen(true); setNode(d) });
+
+
         function mouseovered(active) {
-          return function(d) {
-            d3.select(this).classed("label--active", active);
-            d3.select(d.linkExtensionNode).classed("link-extension--active", active).raise();
-            do d3.select(d.linkNode).classed("link--active", active).raise();
-            while (d = d.parent);
-          };
+            return function (d) {
+                d3.select(this).classed("label--active", active);
+            };
         }
-      }
-      makeChart();
     }
 
-
-
-    render(){   
+    render() {
 
         return <div>
-                    <svg> </svg>
-               </div>
+            <svg> </svg>
+        </div>
     }
 }
--- src/container/Viewer.js
+++ src/container/Viewer.js
@@ -10,39 +10,27 @@ import { AppProvider } from './Store'
 
 
 const styles = therme => ({
-  tree_div: {
-    border: 0,
-    borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(0, 0, 0, .3)',
-    color: 'gray',
-  },
+    tree_div: {
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(0, 0, 0, .3)',
+        width: 'calc(100vw - 32px)'
+    },
 });
 
 
 class ViewerScreen extends React.Component {
 
-    constructor(props){
-        super(props)
-        this.state = {
-            isopen: false,
-        }
-    }
-
-    toggleDrawer = (open) => {
-        this.setState({ isopen : open });
-        console.log(open)
-    };
-
-    render(){
+    render() {
         const { classes } = this.props;
         return (
-            <AppProvider> 
+            <AppProvider>
                 <Grid container spacing={2}>
                     <TreeToolbar />
                     <Grid key={1} item>
                         <div className={classes.tree_div}>
                             <Tree />
-                            <Sidebar isopen={this.state.isopen}/>
+                            <Sidebar isopen={false} />
                         </div>
                     </Grid>
                 </Grid>
