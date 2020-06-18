2091ea7d92dd7d3be8ecef54624258ef190eea2f
Colin <colin.diesh@gmail.com>
2020-06-18 11:38:09 -0400

    Use indentation level 4

--- src/container/Viewer.js
+++ src/container/Viewer.js
@@ -1,43 +1,50 @@
-import React from 'react'
-import { withStyles } from "@material-ui/core/styles";
-import Grid from '@material-ui/core/Grid';
+import React, { useRef, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 
-import Tree from '../components/Tree'
-import Sidebar from '../components/Sidebar'
-import TreeToolbar from '../components/TreeToolbar'
-import { AppProvider } from './Store'
+import Tree from "../components/Tree";
+import Sidebar from "../components/Sidebar";
+import TreeToolbar from "../components/TreeToolbar";
+import { AppProvider } from "./Store";
+import { treeData } from "../components/life";
 
-
-
-const styles = therme => ({
+const useStyles = makeStyles((therme) => ({
     tree_div: {
         border: 0,
         borderRadius: 3,
-        boxShadow: '0 3px 5px 2px rgba(0, 0, 0, .3)',
-        width: 'calc(100vw - 32px)'
+        boxShadow: "0 3px 5px 2px rgba(0, 0, 0, .3)",
+        width: "calc(100vw - 32px)",
     },
-});
+}));
 
+function ViewerScreen(props) {
+    const ref = useRef();
+    const classes = useStyles();
+    const [value, setValue] = useState(treeData);
 
-class ViewerScreen extends React.Component {
-
-    render() {
-        const { classes } = this.props;
-        return (
-            <AppProvider>
-                <Grid container spacing={2}>
-                    <TreeToolbar />
-                    <Grid key={1} item>
-                        <div className={classes.tree_div}>
-                            <Tree />
-                            <Sidebar isopen={false} />
-                        </div>
-                    </Grid>
+    return (
+        <AppProvider>
+            <Grid container spacing={2}>
+                <TreeToolbar />
+                <Grid key={1} item>
+                    <p>Paste a newick format tree below</p>
+                    <textarea
+                        cols={120}
+                        rows={10}
+                        ref={ref}
+                        onChange={(event) => {
+                            setValue(event.target.value);
+                        }}
+                        value={value}
+                    />
+                    <div className={classes.tree_div}>
+                        <Tree treeData={value} />
+                        <Sidebar isopen={false} />
+                    </div>
                 </Grid>
-            </AppProvider>
-        );
-    }
-
+            </Grid>
+        </AppProvider>
+    );
 }
 
-export default withStyles(styles, { withTheme: true })(ViewerScreen)
+export default ViewerScreen;
