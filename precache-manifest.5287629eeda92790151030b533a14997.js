b6f5973d4b811722a4890ba363a3d13df8b6a233
Dr. Shreya Srivastava <shreya2feb@gmail.com>
2020-06-30 20:52:27 +0530

    Updates

---
+++ precache-manifest.5287629eeda92790151030b533a14997.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc978afaf311bc21b5ae927f5a2ee46d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "24562dc45f0ea3f401c0",
+    "url": "./static/js/2.269632d1.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "./static/js/2.269632d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ab4d0d1685db1e84ecd",
+    "url": "./static/js/main.b9fd70cd.chunk.js"
+  },
+  {
+    "revision": "373cbc13e62b614a4df2",
+    "url": "./static/js/runtime-main.f43c0181.js"
+  }
+]);
