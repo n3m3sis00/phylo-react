df275a405b8922e9885e16eadc84403cfe0342f1
Colin <colin.diesh@gmail.com>
2020-07-07 11:14:34 -0400

    Add stockholm format file example

--- src/container/Viewer.js
+++ src/container/Viewer.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import Box from '@material-ui/core/Box'
@@ -9,6 +9,7 @@ import Data from '../components/Data'
 import MetaData from '../components/MetaData'
 import TreeToolbar from '../components/TreeToolbar'
 import { AppProvider } from './Store'
+const Stockholm = require('stockholm-js')
 
 const useStyles = makeStyles(therme => ({
   tree_div: {
@@ -23,7 +24,50 @@ const useStyles = makeStyles(therme => ({
 
 function ViewerScreen(props) {
   const classes = useStyles()
+  useEffect(() => {
+    ;(async () => {
+      const result = await fetch('Lysine.stock')
+      const text = await result.text()
 
+      if (Stockholm.sniff(text)) {
+        let align = Stockholm.parse(text)
+        const rows = align.rows(),
+          columns = align.columns(),
+          names = align.seqname // the order of alignment rows is well-defined
+
+        if (rows) {
+          const newRow = align.seqdata[names[0]].replace(/./g, 'N')
+          align.addRow('NewRow', newRow)
+          align.deleteRow('AE017267.1/95018-94836')
+        }
+
+        console.warn('Tree is ' + align.gf.NH.join(''))
+        console.warn('Consensus structure is ' + align.gc.SS_cons)
+
+        const seq = 'M93419.1/332-511'
+        console.warn('Structure of ' + seq + ' is ' + align.gr.SS[seq])
+        console.warn('First column of ' + seq + ' is ' + align.seqdata[seq][0])
+
+        console.log(align.toString())
+        console.log(align.toFasta())
+      } else {
+        console.error("Doesn't look like Stockholm format")
+      }
+
+      const align2 = Stockholm.fromSeqIndex({
+        row1: 'AAAA',
+        row2: 'GGGG',
+        row3: 'AGAG',
+      })
+      console.log(align2.toString())
+
+      const align3 = Stockholm.fromRowList([
+        ['ancestor', 'AAAA'],
+        ['descendant', 'AAGA'],
+      ])
+      console.log(align3.toFasta())
+    })()
+  }, [])
   return (
     <AppProvider>
       <Grid container spacing={2}>
