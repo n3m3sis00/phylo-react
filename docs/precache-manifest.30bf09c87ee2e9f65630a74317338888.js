24de28e61464207b5cd1199df1d9f2f0e6528855
n3m3sis00 <34513489+n3m3sis00@users.noreply.github.com>
2020-07-29 10:04:59 +0000

    Deploying to gh-pages from  @ f7a3806060c3006cb56718b8a98e39e941164fc5 🚀

---
+++ docs/precache-manifest.30bf09c87ee2e9f65630a74317338888.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc65b463a5aeab1df16ef83a5206c21",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.511b2bee1bff5e885fd7.bundle.js"
+  },
+  {
+    "url": "runtime~main.511b2bee1bff5e885fd7.bundle.js"
+  },
+  {
+    "url": "vendors~main.511b2bee1bff5e885fd7.bundle.js"
+  },
+  {
+    "url": "vendors~main.511b2bee1bff5e885fd7.bundle.js.LICENSE.txt"
+  }
+]);
